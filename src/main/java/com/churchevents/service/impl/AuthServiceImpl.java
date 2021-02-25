c19cc6d7cd8e2f7dfaf93966c050c1cc9f4e8035
Vasil Manavski <vasilmanavski@gmail.com>
2021-02-25 11:08:28 +0100

    email verification exception added

--- src/main/java/com/churchevents/service/impl/AuthServiceImpl.java
+++ src/main/java/com/churchevents/service/impl/AuthServiceImpl.java
@@ -1,6 +1,7 @@
 package com.churchevents.service.impl;
 
 import com.churchevents.model.User;
+import com.churchevents.model.exceptions.EmailNotVerifiedException;
 import com.churchevents.model.exceptions.InvalidArgumentsException;
 import com.churchevents.model.exceptions.InvalidEmailOrPasswordException;
 import com.churchevents.model.exceptions.InvalidUserCredentialsException;
@@ -23,7 +24,13 @@ public class AuthServiceImpl implements AuthService {
                 password == null || password.isEmpty()) {
             throw new InvalidArgumentsException();
         }
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(InvalidEmailOrPasswordException::new);
 
+        if(!user.isEnabled()) {
+            throw new EmailNotVerifiedException();
+
+        }
         return userRepository.findByEmailAndPassword(email, password)
                 .orElseThrow(InvalidUserCredentialsException::new);
     }
